dfbd4b29e736961c233a51765b1bce52571918b2
Gerardo Garcia <g.garciac@outlook.com>
2020-09-24 17:09:50 -0500

    Denug script Sonar cloud

--- sonarcloud/run.sh
+++ sonarcloud/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo "======= Instalando Sonar Scaner"
 sudo dotnet tool install --tool-path=/usr/bin dotnet-sonarscanner --version 4.10.0
+echo "======= Inicializando análisis"
 sudo dotnet sonarscanner begin /o:gerardoat /k:gerardoat_pocdevops /d:sonar.host.url=https://sonarcloud.io /version:$(buildId)
+echo "======= Compilando código"
 sudo dotnet build src --configuration Release
+echo "======= Finalizando análisis"
 sudo dotnet sonarscanner end
