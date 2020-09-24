b914e28fbcf362fe08c54b1c58e8e97d338ac680
Gerardo Garcia <g.garciac@outlook.com>
2020-09-23 22:50:41 -0500

    Correcciones a recuperación de SAS desde Key Vault

--- jmeter/test.sh
+++ jmeter/test.sh
@@ -3,7 +3,6 @@ testFile=$2
 host=$3
 idReporte=$4
 fqdnContenedor=$5
-sasContenedor=$6
 
 echo "======================== PARAMETROS ========================"
 echo "Root path: $rootPath"
@@ -28,29 +27,17 @@ rm -f $T_DIR/test-plan.jtl $T_DIR/jmeter.log  > /dev/null 2>&1
 	-n -t /test/$testFile -l $T_DIR/test-plan.jtl -j $T_DIR/jmeter.log \
 	-e -o $R_DIR
 
-echo "==== LS de $R_DIR ===="
-ls -l $R_DIR
-
-echo "==== LS de $T_DIR ===="
-ls -l $T_DIR
-
-# echo "==== jmeter.log ===="
-# cat $T_DIR/jmeter.log
-
-# echo "==== Raw Test Report ===="
-# cat $T_DIR/test-plan.jtl
-
-echo "==== HTML Test Report ===="
-echo "See HTML test report in $R_DIR/index.html"
+echo "===================== Decodificar SAS"
+SAS_CONTENEDOR_REPORTES=`echo $SAS_CONTENEDOR_REPORTESB64 | base64 --decode`
 
 echo "===================== Descarga de AzCopy"
 wget -O azcopy.tar.gz https://aka.ms/downloadazcopy-v10-linux && tar -xf azcopy.tar.gz --strip-components=1
 echo "===================== Descarga de Index"
-./azcopy copy "$fqdnContenedor/index.html?$sasContenedor" "./index.html"
+./azcopy copy "$fqdnContenedor/index.html?$SAS_CONTENEDOR_REPORTES" "./index.html"
 echo "===================== Carga de reporte a $fqdnContenedor/rep/$idReporte"
-./azcopy copy "./report" "$fqdnContenedor/rep/$idReporte?$sasContenedor" --recursive=true
+./azcopy copy "./report" "$fqdnContenedor/rep/$idReporte?$SAS_CONTENEDOR_REPORTES" --recursive=true
 liReporte="<li><a href='rep/$idReporte/report/index.html'>Build $idReporte</a></li>"
 echo "===================== Actualizando Index con $liReporte"
 echo "$liReporte" >> ./index.html
 echo "===================== Carga de Index"
-./azcopy copy "./index.html" "$fqdnContenedor/index.html?$sasContenedor" --recursive=true
+./azcopy copy "./index.html" "$fqdnContenedor/index.html?$SAS_CONTENEDOR_REPORTES" --recursive=true
