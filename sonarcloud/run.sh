5a016c019720e1ea07d7ab826f498697392efbd4
Gerardo Garcia <g.garciac@outlook.com>
2020-09-24 17:17:37 -0500

    Se parametriza shell de Sonarcloud

--- sonarcloud/run.sh
+++ sonarcloud/run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-echo "======= Instalando Sonar Scaner"
-sudo dotnet tool install --tool-path=/usr/bin dotnet-sonarscanner --version 4.10.0
-echo "======= Inicializando análisis"
-sudo dotnet sonarscanner begin /o:gerardoat /k:gerardoat_pocdevops /d:sonar.host.url=https://sonarcloud.io /version:$(buildId)
+ROOTPATH=$1
+VERSIONANALISIS=$2
+ls -l
+echo "======= Instalando Sonar Scaner Path: $ROOTPATH"
+dotnet tool install --tool-path=$ROOTPATH --version 4.10.0
+ls -l
+echo "======= Inicializando análisis Versión: $VERSIONANALISIS"
+dotnet sonarscanner begin /o:gerardoat /k:gerardoat_pocdevops /d:sonar.host.url=https://sonarcloud.io /version:$VERSIONANALISIS
 echo "======= Compilando código"
-sudo dotnet build src --configuration Release
+dotnet build src --configuration Release
 echo "======= Finalizando análisis"
-sudo dotnet sonarscanner end
+dotnet sonarscanner end
