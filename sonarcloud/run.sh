ae4a31c5e1e4d5a93bef5fd9c7d2dc79348f1b7e
Gerardo Garcia <g.garciac@outlook.com>
2020-09-24 17:40:15 -0500

    sudo

--- sonarcloud/run.sh
+++ sonarcloud/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-ROOTPATH=$1
+ROOTPATH='/usr/bin'
 VERSIONANALISIS=$2
 ls -l
 echo "======= Instalando Sonar Scaner Path: $ROOTPATH"
-dotnet tool install --tool-path=$ROOTPATH dotnet-sonarscanner --version 4.10.0
+sudo dotnet tool install --tool-path=$ROOTPATH dotnet-sonarscanner --version 4.10.0
 ls -l
 echo "======= Inicializando análisis Versión: $VERSIONANALISIS"
-dotnet sonarscanner begin /o:gerardoat /k:gerardoat_pocdevops /d:sonar.host.url=https://sonarcloud.io /version:$VERSIONANALISIS
+sudo dotnet sonarscanner begin /o:gerardoat /k:gerardoat_pocdevops /d:sonar.host.url=https://sonarcloud.io /version:$VERSIONANALISIS
 echo "======= Compilando código"
-dotnet build src --configuration Release
+sudo dotnet build src --configuration Release
 echo "======= Finalizando análisis"
-dotnet sonarscanner end
+sudo dotnet sonarscanner end
