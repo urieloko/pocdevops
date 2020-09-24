dfa2ba2c4a3cc312151db07b74bad84c720ab323
Gerardo Garcia <g.garciac@outlook.com>
2020-09-24 17:46:30 -0500

    Variable para Token

--- sonarcloud/run.sh
+++ sonarcloud/run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 ROOTPATH='/usr/bin'
-VERSIONANALISIS=$2
-ls -l
+VERSIONANALISIS=$1
+
 echo "======= Instalando Sonar Scaner Path: $ROOTPATH"
 sudo dotnet tool install --tool-path=$ROOTPATH dotnet-sonarscanner --version 4.10.0
-ls -l
 echo "======= Inicializando análisis Versión: $VERSIONANALISIS"
 sudo dotnet sonarscanner begin /o:gerardoat /k:gerardoat_pocdevops /d:sonar.host.url=https://sonarcloud.io /version:$VERSIONANALISIS
 echo "======= Compilando código"
 sudo dotnet build src --configuration Release
 echo "======= Finalizando análisis"
-sudo dotnet sonarscanner end
+sudo SONAR_TOKEN=$SONARTOKEN dotnet sonarscanner end
